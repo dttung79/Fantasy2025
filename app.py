24be608ee707cf1b1edce80d2873a86e2080eda5
dttung79 <dttung@gmail.com>
2025-12-09 09:12:30 +0700

    update week 15

--- app.py
+++ app.py
@@ -610,4 +610,4 @@ def cronjob():
 
 ####### main function #######
 if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0', port=5000)
+    app.run(debug = True, host='0.0.0.0', port=5001)
